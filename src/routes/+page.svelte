4520d76fb07e0f07fafcce882bb413f39220e870
EricCHXia <52739449+ericCHxia@users.noreply.github.com>
2023-11-11 15:21:40 +1100

    Refactor Select component and update button styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { page } from "$app/stores";
-	import Select from "$lib/Select.svelte";
+	import { page } from '$app/stores';
+	import Select from '$lib/Select.svelte';
 	interface Quiz {
 		short: string;
 		name: string;
 	}
-	const quiz:Quiz[] = $page.data.quiz;
+	const quiz: Quiz[] = $page.data.quiz;
 	const items = quiz.map((q) => {
 		return { value: q.short, label: q.name };
 	});
-	let value: string=quiz[0].short;
+	let value: string = quiz[0].short;
 </script>
 
 <div class="flex mx-auto">
@@ -17,15 +17,16 @@
 		<div class="flex flex-col space-y-2">
 			<div class="flex-grow">
 				<p class="text-lg font-bold sm:text-base">Quiz</p>
-				<Select items={items} bind:value={value} />
+				<Select {items} bind:value />
 			</div>
 			<div class="flex-grow sm:self-end">
-				<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto">
-					<a href="/{encodeURIComponent(value)}">Start</a>
-				</button>
+				<a
+					href="/{encodeURIComponent(value)}"
+					class="btn block w-full sm:w-auto py-1 text-white bg-indigo-600 hover:bg-indigo-700"
+				>
+					Start
+				</a>
 			</div>
 		</div>
 	</div>
 </div>
-
-
